2220b0364bad7f8fb4fdea416fd97317477f8e84
kmorrisongr <kyle.morrison.gr@dartmouth.edu>
2021-08-09 11:24:29 -0400

    no nvim

--- install.sh
+++ install.sh
@@ -7,6 +7,3 @@ test -d "$HOME/.config" && mkdir "$HOME/.config"
 
 test -d "$HOME/.config/bash" && mkdir "$HOME/.config/bash"
 cp .config/bash/bashenv "$HOME/.config/bash/bashenv"
-
-test -d "$HOME/.config/nvim" && mkdir "$HOME/.config/nvim"
-cp .config/nvim/init.vim "$HOME/.config/nvim/init.vim"
